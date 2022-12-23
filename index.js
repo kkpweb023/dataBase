c32fa8b4b1c855e0963f2bf603a9e2be07e9c9bb
CodeWithKaushal023 <kpandey023@gmail.com>
2022-12-23 14:18:08 +0530

    mongoo connect

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = process.env.PORT || 4000;
 require('./Database/config');
 const Products = require('./Database/ProductSchema');
+
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
 
 
 app.get('/list-Product',async (req,res)=>{
@@ -23,7 +36,7 @@ app.get('/list-Product',async (req,res)=>{
 
 
 
-app.listen(port,() => {
+connectDB().app.listen(port,() => {
   console.log(`from port ${port}`)
 
 })
