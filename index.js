fdb939e99e9de6e014796fa82bc6b2cab993b65f
CodeWithKaushal023 <kpandey023@gmail.com>
2022-12-23 14:37:28 +0530

    d

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000;
-
+require('./Database/config');
+const Products = require('./Database/ProductSchema');
 
 
 app.get('/list-Product',async (req,res)=>{
 
-    let data = "Hello";
+    let data = await Products.find({});
 
+    if(data.length > 0){
         res.send(data)
- 
+    }else{
+        res.send("No data found");
+    }
  
 })
 
