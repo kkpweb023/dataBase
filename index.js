01cb1bbc911b8352437172e9a86715cc82f3580c
CodeWithKaushal023 <kpandey023@gmail.com>
2022-12-29 22:24:34 +0530

    again images upload error

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ app.put('/upload/:_id', upload, async (req, res) => {
 
     let result = await cloudinary.uploader.upload(req.file.path);
     let data = await User.updateOne(
-        { _id: req.params._id }, { image: result.url }
+        { _id: req.params._id }, { image: req.file.path }
     );
     res.send(data)
 
