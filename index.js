7052d8a501f146b02d01d128d61b2019eb83c12a
CodeWithKaushal023 <kpandey023@gmail.com>
2022-12-31 20:26:12 +0530

    Add jwt token

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const Products = require('./Database/ProductSchema');
 const User = require('./Database/RegSchema');
 let bodyParser = require('body-parser');
 let jsonParser = bodyParser.json();
+const jwt = require('jsonwebtoken');
+const jwtKey = 'my-key';
 
 
 /*
@@ -55,7 +57,13 @@ app.post('/register', jsonParser, async (req, res) => {
         let user = await data.save();
         user = user.toObject();
         delete user.password;
-        res.send(user)
+
+        jwt.sign({user},jwtKey,{expiresIn:'3h'},(err,token)=>{
+            if(err){
+                res.send('something went wrong please try later....')
+            }
+            res.send({user,auth:token});
+        })
     }
 })
 
@@ -64,7 +72,13 @@ app.post('/login', jsonParser, async (req, res) => {
     if (req.body.password && req.body.email) {
         const user = await User.findOne(req.body).select('-password');
         if (user) {
-            res.send(user);
+
+            jwt.sign({user},jwtKey,{expiresIn:'3h'},(err,token)=>{
+                if(err){
+                    res.send('something went wrong please try later....')
+                }
+                res.send({user,auth:token});
+            })
         } else {
             res.send("Please signup");
         }
@@ -73,7 +87,7 @@ app.post('/login', jsonParser, async (req, res) => {
     }
 })
 
-app.get('/getID/:email', async (req, res) => {
+app.get('/getID/:email',VerifyToken, async (req, res) => {
 
     const allData = await User.findOne({ email: req.params.email });
 
@@ -84,7 +98,7 @@ app.get('/getID/:email', async (req, res) => {
     }
 })
 
-app.put('/forgotPass/:_id', jsonParser, async (req, res) => {
+app.put('/forgotPass/:_id',VerifyToken, jsonParser, async (req, res) => {
 
     let data = await User.updateOne(
         { _id: req.params._id }, { $set: req.body }
@@ -92,7 +106,7 @@ app.put('/forgotPass/:_id', jsonParser, async (req, res) => {
     res.send(data);
 })
 
-app.put('/changePass/:_id', jsonParser, async (req, res) => {
+app.put('/changePass/:_id',VerifyToken, jsonParser, async (req, res) => {
 
     let data = await User.updateOne(
         { _id: req.params._id }, { $set: req.body }
@@ -101,7 +115,7 @@ app.put('/changePass/:_id', jsonParser, async (req, res) => {
 })
 
 
-app.delete('/:email', async (req, res) => {
+app.delete('/:email',VerifyToken, async (req, res) => {
 
     const user = await User.findOne({ email: req.params.email });
     const data = await User.deleteOne({ email: req.params.email });
@@ -141,13 +155,13 @@ app.put('/remove/:_id', async (req, res) => {
 ///////////////List/////////////
 
 
-app.post('/add-Product', jsonParser, async (req, res) => {
+app.post('/add-Product',VerifyToken,jsonParser, async (req, res) => {
     let data = new Products(req.body);
     let products = await data.save();
     res.send(products)
 })
 
-app.get('/list-Product', async (req, res) => {
+app.get('/list-Product',VerifyToken, async (req, res) => {
     let data = await Products.find({});
     if (data.length > 0) {
         res.send(data)
@@ -156,7 +170,7 @@ app.get('/list-Product', async (req, res) => {
     }
 })
 
-app.get('/update-Product/:_id', async (req, res) => {
+app.get('/update-Product/:_id',VerifyToken,async (req, res) => {
     let data = await Products.find({ _id: req.params._id });
     if (data.length > 0) {
         res.send(data)
@@ -165,7 +179,7 @@ app.get('/update-Product/:_id', async (req, res) => {
     }
 })
 
-app.put('/update-Product/:_id', jsonParser, async (req, res) => {
+app.put('/update-Product/:_id', VerifyToken, jsonParser, async(req, res) => {
     let result = await Products.updateOne(
         { _id: req.params._id },
         { $set: req.body }
@@ -173,13 +187,13 @@ app.put('/update-Product/:_id', jsonParser, async (req, res) => {
     res.send(result);
 })
 
-app.delete('/delete-Product/:_id', async (req, res) => {
+app.delete('/delete-Product/:_id',VerifyToken, async (req, res) => {
 
     let data = await Products.deleteOne({ _id: req.params._id });
     res.send(data);
 })
 
-app.get('/search/:key', async (req, res) => {
+app.get('/search/:key',VerifyToken, async (req, res) => {
 
     const data = await Products.find({
         '$or': [
@@ -191,6 +205,39 @@ app.get('/search/:key', async (req, res) => {
     });
     res.send(data);
 })
+
+
+////////////////// Verify Token ///////////////////////
+
+
+function VerifyToken(req,res,next){
+    let token = req.headers['authorization'];
+    if(token){
+        token=token.split(' ')[1];
+        jwt.verify(token,jwtKey,(err,valid)=>{
+              if(err){
+                res.status(401).send('Please add valid token')
+              }else{
+                next();
+              }
+        })
+    }else{
+        res.status(403).send('Please add token with header')
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
