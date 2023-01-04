c930db32af6131169b393426172df49ee9a239db
CodeWithKaushal023 <kpandey023@gmail.com>
2023-01-04 15:42:57 +0530

    mern data.

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ app.get('/list-Product',VerifyToken, async (req, res) => {
     if (data.length > 0) {
         res.send(data)
     } else {
-        res.send("No data found",data);
+        res.send(data);
        
     }
 })
